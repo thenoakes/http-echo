ed6881cc28c1c63037aa71eeed1ee58b498e4fd1
Matt Noakes <mattnoakes@me.com>
2020-05-31 17:37:20 +0100

    Prettier

--- routes/index.ts
+++ routes/index.ts
@@ -1,51 +1,50 @@
-import { opine, Request, Response, parseMultipartRelated } from '../deps.ts';
+import { opine, Request, Response, parseMultipartRelated } from "../deps.ts";
 
 const router = opine.Router();
 
-const EMPTY = '';
-const BREAK = '\r\n';
+const EMPTY = "";
+const BREAK = "\r\n";
 
 // ==== QUICK AND DIRTY CONFIGURATION ====
 
-/** 
- * Should the output of large POSTs in the console be snipped 
+/**
+ * Should the output of large POSTs in the console be snipped
  * (setting to false can cause performance issues for large payloads).
  * Messages are always saved in full to a file.
  * */
-const snipLargeContent = true;    
+const snipLargeContent = true;
 
 /** The number of characters to cap a large payload to, when snipping */
 const maxLength = 10000;
 
 async function echoMultipartPost(req: Request, res: Response) {
-
   /** A function for writing out to the console (optionally snipped) and to a new file */
-  const echo = await (async () => { 
-
+  const echo = await (async () => {
     const writeToLog = await (async () => {
-      const log_file = await Deno.open(`post${(new Date()).getTime()}.http`, { create: true, append: true });
+      const log_file = await Deno.open(`post${new Date().getTime()}.http`, {
+        create: true,
+        append: true,
+      });
       const resourceId = log_file.rid;
-      return async (output : string) => {
+      return async (output: string) => {
         const buffer: Uint8Array = new TextEncoder().encode(output + BREAK);
         await Deno.write(resourceId, buffer);
-      }
+      };
     })();
- 
-    return async (output: string) => {
-      
-        await writeToLog(output);
-        const contentLength = output.length;
-        if (!snipLargeContent || contentLength <= maxLength) {
-            console.log(output);
-        }
-        else {
-            console.log(
-                `${output.slice(0, maxLength / 2)}` +
-                ` ${BREAK} < ... snip ... > ${BREAK} ` + 
-                `${output.slice(contentLength - maxLength / 2, contentLength)}`);
-        } 
 
-    }
+    return async (output: string) => {
+      await writeToLog(output);
+      const contentLength = output.length;
+      if (!snipLargeContent || contentLength <= maxLength) {
+        console.log(output);
+      } else {
+        console.log(
+          `${output.slice(0, maxLength / 2)}` +
+            ` ${BREAK} < ... snip ... > ${BREAK} ` +
+            `${output.slice(contentLength - maxLength / 2, contentLength)}`
+        );
+      }
+    };
   })();
 
   const bodyArray = await Deno.readAll(req.body);
@@ -54,49 +53,49 @@ async function echoMultipartPost(req: Request, res: Response) {
   console.log(EMPTY);
   await echo(`POST ${req.url} HTTP/1.1`);
 
-  let contentType : string | null = null;
+  let contentType: string | null = null;
 
   // Print the request headers and obtain Content-Type
   for (let header of req.headers.keys()) {
-      await echo(header + ": " + req.headers.get(header));
-      if (header.toLowerCase() === 'content-type') {
-        contentType = req.headers.get(header); //.toLowerCase();
-      }
+    await echo(header + ": " + req.headers.get(header));
+    if (header.toLowerCase() === "content-type") {
+      contentType = req.headers.get(header); //.toLowerCase();
+    }
   }
 
   const boundary = (() => {
-    if (!contentType) return '';
+    if (!contentType) return "";
     const BOUNDARY_PARSER = /;\s*[Bb][Oo][Uu][Nn][Dd][Aa][Rr][Yy]=(.*?)\s*(;|$)/i;
     const parsed = BOUNDARY_PARSER.exec(contentType);
-    let boundary = (parsed && parsed.length) ? (parsed[1] || parsed[2]) : "";
+    let boundary = parsed && parsed.length ? parsed[1] || parsed[2] : "";
     return boundary.replace(/\"/g, "");
   })();
 
   const parsedRequest = await parseMultipartRelated(bodyString, boundary);
 
   for (let part of parsedRequest) {
-
-    await echo(BREAK + '--' + boundary);
+    await echo(BREAK + "--" + boundary);
 
     // Print the part's headers
     for (let header in part.headers) {
-      await echo(header + ': ' + part.headers[header]);
+      await echo(header + ": " + part.headers[header]);
     }
-    
+
     await echo(BREAK);
     await echo(part.content);
-
   }
-  
-  await echo(BREAK + '--' + boundary + '--');
+
+  await echo(BREAK + "--" + boundary + "--");
   res.sendStatus(200);
 
-  return console.log(`${BREAK}^^ NON-MULTIPART POST LOGGED @ ${new Date()} ^^${BREAK}`);
+  return console.log(
+    `${BREAK}^^ MULTIPART POST LOGGED @ ${new Date()} ^^${BREAK}`
+  );
 }
 
-router.post('/multipart', echoMultipartPost);
+router.post("/multipart", echoMultipartPost);
 
-router.post('/ping', (req: Request, res: Response) => {
+router.post("/ping", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
