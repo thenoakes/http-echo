0c6f399b2d4aa788508da3d9b91987a870867371
Matt Noakes <mattnoakes@me.com>
2021-04-27 20:02:14 +0100

    Remove branches from deno imports and add readme

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ Beginnings of a deno port of a node app which just prints out raw incoming HTTP 
 ```
 deno run --allow-net --allow-write app.ts
 ```
+
+@ `master`:
+
+`make test`
+`make run`
+
+Endpoints:
+
+`/ping` for healthcheck
+
+`/raw` to just echo back the raw request
+
+`/multipart` to (fairly pointlessly) take a multipart request apart and put it back together :)
--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as opine from "https://deno.land/x/opine@main/mod.ts";
-export { Request, Response, NextFunction } from "https://deno.land/x/opine@main/src/types.ts";
+export * as opine from "https://deno.land/x/opine/mod.ts";
+export type { Request, Response, NextFunction } from "https://deno.land/x/opine/src/types.ts";
 export { parseMultipartRelated } from 'https://raw.githubusercontent.com/thenoakes/multipart-related-parser/master/mod.ts';
